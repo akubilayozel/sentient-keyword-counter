d8a1718f83634177ac0ff0b9a797d96d448c4b78
akubilayozel <84475288+akubilayozel@users.noreply.github.com>
2025-11-10 19:34:13 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{maxWidth: 920, margin: "28px auto", fontFamily:"Inter, system-ui, sans-serif"}}>
+      <h1>Sentient Keyword Counter</h1>
+    </main>
+  );
+}
